6fdc9b2d4a4ea014bb42bd6e99bd6edbf4cfc75d
Lmyfreeman <2515946609@qq.com>
2020-05-21 19:26:53 +0800

    求所有子数组的和的最大值

---
+++ src/maxSubArray.java
@@ -0,0 +1,16 @@
+public class maxSubArray {
+    private static int maxSubArray(int[] nums) {
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += Math.max(nums[i - 1], 0);
+            res = Math.max(res, nums[i]);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int m = maxSubArray(arr);
+        System.out.println(m);
+    }
+}
