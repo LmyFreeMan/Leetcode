dcf5c0086525d08044edbba31c07e6aa63c1ac22
Lmyfreeman <2515946609@qq.com>
2020-05-26 11:35:22 +0800

    扑克牌中的顺子

---
+++ src/isStraight.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class isStraight {
+    private static boolean isStraight(int[] nums) {
+        Set<Integer> repeat = new HashSet<>();
+        int max = 0, min = 14;
+        for (int num : nums) {
+            if (num == 0) continue; // 跳过大小王
+            max = Math.max(max, num); // 最大牌
+            min = Math.min(min, num); // 最小牌
+            if (repeat.contains(num)) return false; // 若有重复，提前返回 false
+            repeat.add(num); // 添加此牌至 Set
+        }
+        return max - min < 5; // 最大牌 - 最小牌 < 5 则可构成顺子
+    }
+
+    public static void main(String[] args) {
+        int[] name = {1, 2, 3, 4, 5};
+        System.out.println(isStraight(name));
+    }
+}
